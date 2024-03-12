9a732db59bd9612cd422bff2840bea3cadc7f5c4
HaizalChew <93971416+HaizalChew@users.noreply.github.com>
2024-03-12 15:01:32 +0900

    Initialized Flask backend & React Frontend

---
+++ flask-server/server.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/settings")
+def settings():
+    return {"users": ["IP Address", "Port", "Room"]}
+
+if __name__ == "__main__":
+    app.run(debug = True)
